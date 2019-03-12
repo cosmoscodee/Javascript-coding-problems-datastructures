73adcca378aace58a35497955f210e6b517e38c8
gusdn6526 <gusdn6526@gmail.com>
2019-03-12 10:40:49 +0900

    Create a directory

---
+++ basic-algorithms/vowelCount.js
@@ -0,0 +1,16 @@
+// vowelCount('ABDCE');  / 2
+// You can count the Vowel in array
+
+function vowelCount(str) {
+	var str = str.split('');
+	var count = 0;
+	var regexp = /[A,E,I,O,U]/gi;
+	for(var i=0; i<str.length; i++) {
+    	if(str[i].match(regexp)) {
+	    count++;
+		}
+	}
+	return count;
+}
+
+console.log(count);
---
+++ datastructures/stack.js
@@ -0,0 +1,32 @@
+// Implementation of Stack in JavaScript
+// We would be implementing Stack Data Structure in Javascript.
+// Stack is a very useful data structure and has a wide range of application.
+
+// Functions to be implemented- push(item) pop() peek() isEmpty() printStack()
+
+
+function push(item) {
+
+}
+
+function pop() {
+
+}
+
+function peek() {
+
+}
+
+function isEmpty() {
+
+}
+
+function printStack() {
+
+}
+
+function init() {
+	
+}
+
+init();
